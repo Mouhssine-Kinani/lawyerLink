206400852624eb958151204517ed7df3ecd7ea2c
Khalfi Aymane <khalfiaymane@gmail.com>
2026-04-14 16:32:34 +0100

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.core.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
+
+
 from app.core.config import settings
 from app.auth import router as auth_router
 
