25d66a0008443d1736c77bdee65b3ff56cbd7ee1
Khalfi Aymane <khalfiaymane@gmail.com>
2026-04-25 19:09:01 +0100

    merge: fixed main.py conflict

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,10 @@ from app.core.config import settings
 from app.auth import router as auth_router
 from app.user import router as user_router
 from app.chat import router as chat_router
+
+from app.reservation import router as reservation_router
 from app.recommendation import router as recommendation_router
+
 
 #models
 import app.user.model
@@ -44,6 +47,7 @@ app.add_middleware(
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(chat_router.router)
+app.include_router(reservation_router.router)
 app.include_router(recommendation_router.router)
 
 
