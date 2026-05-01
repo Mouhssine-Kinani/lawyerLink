3fdbd3bbb76077dbdc0f06a6531317409bbf8a62
Khalfi Aymane <khalfiaymane@gmail.com>
2026-05-01 21:08:34 +0100

    feature:added review endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,9 +8,9 @@ from app.core.config import settings
 from app.auth import router as auth_router
 from app.user import router as user_router
 from app.chat import router as chat_router
-
 from app.reservation import router as reservation_router
 from app.recommendation import router as recommendation_router
+from app.review import router as review_router
 
 
 #models
@@ -49,7 +49,7 @@ app.include_router(user_router.router)
 app.include_router(chat_router.router)
 app.include_router(reservation_router.router)
 app.include_router(recommendation_router.router)
-
+app.include_router(review_router.router)
 
 @app.get("/")
 def index():
